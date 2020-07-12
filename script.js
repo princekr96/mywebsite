5732c1ca0d4554a7257f97f6f6e2673d98782fb0
unknown <princekr700@gmail.com>
2020-07-12 14:12:17 +0530

    added the mouse out fn

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ x.addEventListener("mouseover", myfunction);
 x.addEventListener("mouseout", my2ndFunction);
 
 function myfunction() {
-    document.getElementById('hyper').innerHTML += 'Let\'s Find Out ';
+    document.getElementById('hyper').innerHTML = 'Let\'s Find Out ';
+}
+
+function my2ndFunction() {
+    document.getElementById('hyper').innerHTML = '';
 }
